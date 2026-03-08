9bfbced08f91fcb8821bc559408b1af459aa34bc
9a10bushrafatimashaikh-art <9a10bushrafatimashaikh@gmail.com>
2026-03-09 03:48:52 +0530

    Delete exp141.py

--- exp141.py
+++
@@ -1,9 +0,0 @@
-try:
-    num1 = int(input("Enter first value: "))
-    num2 = int(input("Enter second value: "))
-
-    division = num1 / num2
-    print("Division:", division)
-
-except ZeroDivisionError:
-    print("Number cannot be divided by zero.")
